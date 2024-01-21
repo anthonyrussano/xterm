6a51974a451713b9dcfc5f3df84af9a19a978ad0
Anthony Russano <me@anthonyrussano.com>
2024-01-21 15:32:15 -0800

    migrate from flask to nodejs

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+wss.on('connection', function connection(ws) {
+    console.log('Client connected');
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        ws.send(`echo from server: ${message}\r`);
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function listening() {
+    console.log(`Listening on ${PORT}`);
+});
